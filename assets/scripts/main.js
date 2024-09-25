e14ccd04475026eab7cb472629e0b8d2ef3a46ad
Jérôme DUBOS <jeromedubos67@gmail.com>
2024-09-25 17:14:02 +0200

    Code du jeu (reste a coder reponseCorrecte)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,25 +1,47 @@
 const btnValider = document.getElementById("btnValider")
+let pokemon = document.querySelector(".zonePokemon")
+let score = document.querySelector(".zoneScore span")
+let reponse = document.getElementById("inputEcriture")
+let compteurScore = 0
 let inputEcriture = document.getElementById("inputEcriture")
 
+function initScore() {
+    score.innerHTML = compteurScore
+}
 
 function getRandom(min, max) {
-    min = 0;
+    min = 1;
     max = "https://tyradex.vercel.app/api/v1/pokemon/".length;
     return Math.floor(Math.random() * (max + 1));
 }
 
 function getPokemon() {
-    fetch(`https://tyradex.vercel.app/api/v1/pokemon/${getRandom()}`).then((response) =>{
-        response.json().then((data)=>{
+    fetch(`https://tyradex.vercel.app/api/v1/pokemon/${getRandom()}`).then((response) => {
+        response.json().then((data) => {
             document.querySelector(".zonePokemon").src = data.sprites.regular
-        })  
+            let reponseCorrecte = data.name.fr
+            console.log(reponseCorrecte);
+        })
     })
 }
+let reponseCorrecte = "test"
 
+function verifierReponse() {
+    let reponseDonnee = inputEcriture.value
+    console.log(reponseDonnee);
+    if (reponseDonnee === reponseCorrecte){
+        compteurScore++
+        initScore()
+    } else {
+        window.alert(`La bonne réponse était ${reponseCorrecte}`)
+    }
+    inputEcriture.value = ''
+}
 
 btnValider.addEventListener('click', () => {
+    verifierReponse()
     getPokemon()
 })
 
-
 getPokemon()
+
